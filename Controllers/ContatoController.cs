4e8d6ce935fa1d10d3b76473a6463e786fc074ca
Matheus Ferreira <matheusscfr@gmail.com>
2023-11-23 13:51:24 -0300

    Initial commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleWeb.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirm()
+        {
+            return View();
+        }
+
+
+    }
+}
